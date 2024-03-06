e6e8e75f50f1e8ccefb8da90393c49387920b414
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-03-06 09:40:07 +0000

    delete file don't need for project airbnb console

--- UUID.py
+++
@@ -1,6 +0,0 @@
-import uuid
-
-uuid_random = uuid.uuid4() #permet de creer un UUID aléatoire en format uuid.UUID
-uuid_str = str(uuid_random) #transforme le type de UUID en str
-print("UUID aléatoire (version 4) :", uuid_str) #affiche le UUID
-print("UUID aléatoire (version 4) :", type(uuid_str)) #affiche le type de UUID

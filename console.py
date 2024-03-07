0be419d5ba508b9ddf61bb4f44bc0a14ee299a8e
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-03-07 14:33:45 +0000

    import class User in console

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.amenity import Amenity
+from models.user import User
 from models.engine.file_storage import FileStorage
 
 

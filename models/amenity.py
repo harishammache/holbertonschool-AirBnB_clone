3082ed7ac60e077ca17f46f16567c3d4677cbee3
harishammache <hammache_haris@hotmail.fr>
2024-03-07 09:04:29 +0000

    Amenity class that inherits from BaseModel.

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""models/amenity.py"""
+
+from models.base_model import BaseModel
+
+
+class amenaty(BaseModel):
+    """Amenity class that inherits from BaseModel."""
+
+    name = ""
